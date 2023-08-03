0731b5574a46af92206453ca27d87087a013df62
hunggahot <hunggahot@gmail.com>
2023-08-03 12:58:21 +0700

    routing config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import './App.scss';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+
 function App() {
     return (
         <div>
-            <HeaderComponent />
-            <ListEmployeeComponent />
-            <FooterComponent />
+            <BrowserRouter>
+                <HeaderComponent />
+                <Routes>
+                    <Route index element={<ListEmployeeComponent />} />
+                    <Route path="/" element={<ListEmployeeComponent />} />
+                    <Route path="/employees" element={<ListEmployeeComponent />} />
+                    <Route path="*" element={<ListEmployeeComponent />} />
+                </Routes>
+                <FooterComponent />
+            </BrowserRouter>
         </div>
     );
 }
